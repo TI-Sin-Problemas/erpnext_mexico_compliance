8544b9177408b71b7cd88dde90c3f3803ec4e7b9
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-12-02 03:03:54 -0600

    Add Product/Service linked field to product

--- mexican_accounting/hooks.py
+++ mexican_accounting/hooks.py
@@ -42,7 +42,7 @@ app_license = "MIT"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Generators
@@ -56,8 +56,8 @@ app_license = "MIT"
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "mexican_accounting.utils.jinja_methods",
-#	"filters": "mexican_accounting.utils.jinja_filters"
+# 	"methods": "mexican_accounting.utils.jinja_methods",
+# 	"filters": "mexican_accounting.utils.jinja_filters"
 # }
 
 # Installation
@@ -83,11 +83,11 @@ app_license = "MIT"
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -95,7 +95,7 @@ app_license = "MIT"
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
@@ -103,32 +103,32 @@ app_license = "MIT"
 # Hook on document methods and events
 
 # doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
 # }
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"mexican_accounting.tasks.all"
-#	],
-#	"daily": [
-#		"mexican_accounting.tasks.daily"
-#	],
-#	"hourly": [
-#		"mexican_accounting.tasks.hourly"
-#	],
-#	"weekly": [
-#		"mexican_accounting.tasks.weekly"
-#	],
-#	"monthly": [
-#		"mexican_accounting.tasks.monthly"
-#	],
+# 	"all": [
+# 		"mexican_accounting.tasks.all"
+# 	],
+# 	"daily": [
+# 		"mexican_accounting.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"mexican_accounting.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"mexican_accounting.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"mexican_accounting.tasks.monthly"
+# 	],
 # }
 
 # Testing
@@ -140,14 +140,14 @@ app_license = "MIT"
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "mexican_accounting.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "mexican_accounting.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "mexican_accounting.task.get_dashboard_data"
+# 	"Task": "mexican_accounting.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -159,29 +159,31 @@ app_license = "MIT"
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"mexican_accounting.auth.validate"
+# 	"mexican_accounting.auth.validate"
 # ]
+
+fixtures = ["Custom Field"]
