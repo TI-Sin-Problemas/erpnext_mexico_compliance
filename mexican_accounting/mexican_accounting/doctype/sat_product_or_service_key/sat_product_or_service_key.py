54e9ae0b482593054963f4a5f213060d13bff6bd
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-12-02 01:56:30 -0600

    Add Product/Service Key Doctype

---
+++ mexican_accounting/mexican_accounting/doctype/sat_product_or_service_key/sat_product_or_service_key.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Alfredo Altamirano and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SATProductorServiceKey(Document):
+    """SAT's Product or Service Key"""
