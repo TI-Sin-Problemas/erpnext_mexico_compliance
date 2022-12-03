b0dbcc46063360c6a063e29e8ea91936367da46e
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-12-03 17:04:06 -0600

    Add SAT UOM Key DocType

---
+++ mexican_accounting/mexican_accounting/doctype/sat_uom_key/sat_uom_key.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Alfredo Altamirano and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class SATUOMKey(Document):
+	pass
---
+++ mexican_accounting/mexican_accounting/doctype/sat_uom_key/test_sat_uom_key.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Alfredo Altamirano and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSATUOMKey(FrappeTestCase):
+	pass
