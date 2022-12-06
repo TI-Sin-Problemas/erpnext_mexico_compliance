7fb3d0aaa72cfd717618a6d7a9d555e25c6cd604
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-12-06 00:00:17 -0600

    Add SAT Tax System Doctype

---
+++ mexico_compliance/mexico_compliance/doctype/sat_tax_system/sat_tax_system.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, TI Sin Problemas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SAT Tax System', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ mexico_compliance/mexico_compliance/doctype/sat_tax_system/sat_tax_system.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) 2022, TI Sin Problemas and contributors
+For license information, please see license.txt
+"""
+# import frappe
+from frappe.model.document import Document
+
+
+class SATTaxSystem(Document):
+    """SAT's Tax System"""
+
+    def before_save(self):
+        """Set DocType key name"""
+        self.key_name = f"{self.key} - {self.description}"[:140]
