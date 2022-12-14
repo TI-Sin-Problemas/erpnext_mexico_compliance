320ad982fbb52d2bad2d02075b9c9db70c8faeca
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-12-14 13:29:53 -0600

    Add Sales Inovice CFDI Description

--- mexico_compliance/overrides/sales_invoice_item.py
+++ mexico_compliance/overrides/sales_invoice_item.py
@@ -3,6 +3,7 @@ Copyright (c) 2022, TI Sin Problemas and contributors
 For license information, please see license.txt
 """
 import frappe
+from frappe import _
 from erpnext.accounts.doctype.sales_invoice_item import sales_invoice_item
 
 
@@ -20,3 +21,27 @@ class SalesInvoiceItem(sales_invoice_item.SalesInvoiceItem):
         if not self.item_code:
             return None
         return frappe.get_doc("Item", self.item_code)
+
+    def get_service_duration(self):
+        """Returns a string with From and To dates of services"""
+        start_date = ""
+        if self.service_start_date:
+            start_date = _("From {}").format(self.service_start_date)
+
+        end_date = ""
+        if self.service_end_date:
+            start_date = _("To {}").format(self.service_end_date)
+
+        return f"{start_date} {end_date}".strip()
+
+    def get_cfdi_description(self):
+        """Returns description ready to stamp a CFDI"""
+        description = f"{self.item_name}"
+        if self.item_name != self.description:
+            description += f" - {self.description}"
+
+        service_dates = self.get_service_duration()
+        if service_dates:
+            description += f" ({service_dates})"
+
+        return description.strip()
