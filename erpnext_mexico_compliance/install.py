8397ee6ed35aff8874f1a4babf08e06eb565e3e3
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-06-25 23:47:54 -0600

    chore(install): add logger

--- erpnext_mexico_compliance/install.py
+++ erpnext_mexico_compliance/install.py
@@ -6,14 +6,14 @@ from frappe.core.doctype.data_import.data_import import import_file
 
 from .hooks import app_name
 
+logger = frappe.logger("erpnext_mexico_compliance.install")
 
 def after_sync():
     """Run tasks after migration sync"""
-    print("Importing SAT Product or Service Key data...")
+    logger.info("Importing SAT Product or Service Key data...")
     fixtures_directory = "fixtures_csv"
     files = sorted(os.listdir(frappe.get_app_path(app_name, fixtures_directory)))
     for file in files:
-        print("Loading", f"{file}...")
+        logger.info(f"Loading {file}...")
         file_path = frappe.get_app_path(app_name, fixtures_directory, file)
         import_file("SAT Product or Service Key", file_path, "Insert", console=True)
-        print("")
