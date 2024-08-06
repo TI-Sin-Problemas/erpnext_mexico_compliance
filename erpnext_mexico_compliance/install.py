89dd42b25f748a737ff07d02bd100f9794241fea
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-08-05 21:49:40 -0600

    chore: resolve pylint logging-fstring-interpolation

--- erpnext_mexico_compliance/install.py
+++ erpnext_mexico_compliance/install.py
@@ -1,4 +1,5 @@
 """Installation tasks"""
+
 import os
 
 import frappe
@@ -8,12 +9,13 @@ from .hooks import app_name
 
 logger = frappe.logger("erpnext_mexico_compliance.install")
 
+
 def after_sync():
     """Run tasks after migration sync"""
     logger.info("Importing SAT Product or Service Key data...")
     fixtures_directory = "fixtures_csv"
     files = sorted(os.listdir(frappe.get_app_path(app_name, fixtures_directory)))
     for file in files:
-        logger.info(f"Loading {file}...")
+        logger.info("Loading %s...", file)
         file_path = frappe.get_app_path(app_name, fixtures_directory, file)
         import_file("SAT Product or Service Key", file_path, "Insert", console=True)
