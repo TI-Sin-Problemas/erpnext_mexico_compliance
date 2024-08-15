4bf9b706d6f8562f6f51eb273e4fa4f859fa050a
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-08-14 20:12:20 -0600

    feat(cfdi use): add tax regime child table

--- erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_cfdi_use/sat_cfdi_use.py
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_cfdi_use/sat_cfdi_use.py
@@ -8,6 +8,21 @@ from frappe.model.document import Document
 
 
 class SATCFDIUse(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from erpnext_mexico_compliance.erpnext_mexico_compliance.doctype.sat_cfdi_use_tax_regime.sat_cfdi_use_tax_regime import SATCFDIUseTaxRegime
+        from frappe.types import DF
+
+        description: DF.Data
+        enabled: DF.Check
+        key: DF.Data
+        key_name: DF.Data | None
+        tax_regimes: DF.Table[SATCFDIUseTaxRegime]
+    # end: auto-generated types
     """SAT's CFDI Use"""
 
     def before_save(self):
--- erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_tax_regime/sat_tax_regime.py
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_tax_regime/sat_tax_regime.py
@@ -7,7 +7,20 @@ from frappe.model.document import Document
 
 
 class SATTaxRegime(Document):
-    """SAT's Tax System"""
+    """SAT's Tax Regime"""
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        description: DF.Data
+        enabled: DF.Check
+        key: DF.Data
+        key_name: DF.Data | None
+    # end: auto-generated types
 
     def before_save(self):
         """Set DocType key name"""
