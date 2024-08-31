b9c9bb359675d176ba78cf963cde2cb61ea2364c
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-08-30 18:40:50 -0600

    fix(cfdi stamping settings): error when saving

--- erpnext_mexico_compliance/ws_client/__init__.py
+++ erpnext_mexico_compliance/ws_client/__init__.py
@@ -7,13 +7,17 @@ from . import client
 from .exceptions import WSClientException, WSExistingCfdiException
 
 
-def get_ws_client() -> client.WSClient:
+def get_ws_client(settings=None) -> client.WSClient:
     """Retrieves a WSClient instance based on the current CFDI Stamping Settings.
+
+    Args:
+        settings (CFDIStampingSettings, optional): The CFDI Stamping Settings document. Defaults to None.
 
     Returns:
         client.WSClient: A WSClient instance configured with the current API key and operation mode.
     """
-    settings = frappe.get_single("CFDI Stamping Settings")
+    if not settings:
+        settings = frappe.get_single("CFDI Stamping Settings")
     api_key = settings.get_api_key()
     mode_map = {
         0: client.WSClient.OperationMode.PROD,
