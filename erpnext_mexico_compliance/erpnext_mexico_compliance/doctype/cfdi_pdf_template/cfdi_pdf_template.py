b21f718c88bcebe7bd6108bcd40020be40a4ada1
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2025-07-02 06:16:20 +0000

    feat: implement example PDF generation from sample CFDI XML

--- erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/cfdi_pdf_template/cfdi_pdf_template.py
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/cfdi_pdf_template/cfdi_pdf_template.py
@@ -2,11 +2,12 @@
 For license information, please see license.txt"""
 
 import frappe
-from erpnext_mexico_compliance.hooks import app_name
-from erpnext_mexico_compliance.utils import qr_as_base64
 from frappe.model.document import Document
 from frappe.utils.pdf import get_pdf
 from satcfdi.cfdi import CFDI
+
+from erpnext_mexico_compliance.hooks import app_name
+from erpnext_mexico_compliance.utils import qr_as_base64
 
 
 class CFDIPDFTemplate(Document):
@@ -47,19 +48,34 @@ class CFDIPDFTemplate(Document):
             body = f"<body>{self.body}</body>"
         return f"<html>{head}{body}</html>"
 
-    def get_rendered_example(self):
-        """Renders the PDF template with a sample CFDI and returns it as rendered HTML.
+    def get_rendered_pdf(self, xml: str) -> bytes:
+        """Renders the PDF template with the given XML and returns it as a PDF.
 
-        The sample CFDI is taken from the `examples/cfdi/ingreso.xml` file in the app directory.
-        The QR code is generated from the verification URL of the sample CFDI.
+        Args:
+            xml (str): The XML of the CFDI to render.
 
         Returns:
-            str: The rendered HTML template.
+            bytes: The rendered PDF.
         """
-        example_file_path = f"{frappe.get_app_path(app_name)}/examples/cfdi/ingreso.xml"
-        cfdi = CFDI.from_file(example_file_path)
+        cfdi = CFDI.from_string(xml.encode("utf-8"))
         qr = qr_as_base64(cfdi.verifica_url)
-        return frappe.render_template(self.template, {"cfdi": cfdi, "qr": qr})
+        rendered = frappe.render_template(self.template, {"cfdi": cfdi, "qr": qr})
+        return get_pdf(rendered)
+
+    def get_example_pdf(self):
+        """Generates an example PDF using a sample CFDI XML file.
+
+        This method reads a sample CFDI XML file from the application's examples directory,
+        renders it using the PDF template, and returns the resulting PDF as bytes.
+
+        Returns:
+            bytes: The rendered example PDF.
+        """
+
+        example_file_path = f"{frappe.get_app_path(app_name)}/examples/cfdi/ingreso.xml"
+        with open(example_file_path, "r") as f:
+            xml = f.read()
+        return self.get_rendered_pdf(xml)
 
 
 @frappe.whitelist()
@@ -71,5 +87,5 @@ def print_example(docname):
     """
     doc = frappe.get_doc("CFDI PDF Template", docname)
     frappe.local.response.filename = f"{doc.title}.pdf"
-    frappe.local.response.filecontent = get_pdf(doc.get_rendered_example())
+    frappe.local.response.filecontent = doc.get_example_pdf()
     frappe.local.response.type = "pdf"
