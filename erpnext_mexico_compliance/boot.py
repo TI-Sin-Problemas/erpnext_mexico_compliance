dfd10b98cbf42323fd60adb8361c571a1bb93009
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2025-07-07 01:35:05 +0000

    feat(boot): implement caching for subscription details retrieval in boot session

--- erpnext_mexico_compliance/boot.py
+++ erpnext_mexico_compliance/boot.py
@@ -2,9 +2,24 @@
 For license information, please see license.txt"""
 
 from frappe.types.frappedict import _dict
+from frappe.utils.caching import redis_cache
 
 from .hooks import app_name
 from .ws_client import get_ws_client
+
+
+@redis_cache(ttl=43200)  # Cache for 12 hours
+def _get_subscription_details():
+    """Retrieves the app subscription details for the current instance.
+
+    The subscription details are retrieved using the CFDI Web Service.
+    The response is cached for 12 hours using Redis.
+
+    Returns:
+        dict: The subscription details.
+    """
+    ws = get_ws_client()
+    return ws.get_subscription_details()
 
 
 def boot_session(bootinfo: _dict):
@@ -20,6 +35,5 @@ def boot_session(bootinfo: _dict):
     Returns:
         None
     """
-    ws = get_ws_client()
-    subscription = ws.get_subscription_details()
+    subscription = _get_subscription_details()
     bootinfo[app_name] = {"has_subscription": subscription.has_subscription}
