50739a7818774f15b41ced84adc6e4d11145c3de
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2025-07-25 03:40:46 +0000

    feat(mexico_compliance): Adds auto-generated type hints to DocTypes

--- erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_product_or_service_key/sat_product_or_service_key.py
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_product_or_service_key/sat_product_or_service_key.py
@@ -8,6 +8,24 @@ from frappe.model.document import Document
 
 
 class SATProductorServiceKey(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        description: DF.Data
+        enabled: DF.Check
+        is_group: DF.Check
+        key: DF.Data
+        key_name: DF.Data | None
+        lft: DF.Int
+        old_parent: DF.Link | None
+        parent_sat_product_or_service_key: DF.Link | None
+        rgt: DF.Int
+    # end: auto-generated types
     """SAT's Product or Service Key"""
 
     def before_save(self):
--- erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_uom_key/sat_uom_key.py
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/sat_uom_key/sat_uom_key.py
@@ -6,6 +6,20 @@ from frappe.model.document import Document
 
 
 class SATUOMKey(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        description: DF.SmallText | None
+        enabled: DF.Check
+        key: DF.Data
+        key_name: DF.Data | None
+        uom_name: DF.Data
+    # end: auto-generated types
     def before_save(self):
         """Set DocType Name"""
         self.key_name = f"{self.key} - {self.uom_name}"[:140]
