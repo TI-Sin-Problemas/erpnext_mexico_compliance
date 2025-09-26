e083f932a3e88e5687e55e873bbd22541433e255
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2025-09-26 21:35:57 +0000

    feat(sat): Adds SAT catalog synchronization

---
+++ erpnext_mexico_compliance/sat/__init__.py
@@ -0,0 +1,10 @@
+"""Copyright (c) 2025, TI Sin Problemas and contributors
+For license information, please see license.txt
+"""
+
+from .catalogs import CatalogManager
+
+
+def update_sat_catalogs():
+    manager = CatalogManager()
+    manager.update_doctype("SAT Relationship Type")
