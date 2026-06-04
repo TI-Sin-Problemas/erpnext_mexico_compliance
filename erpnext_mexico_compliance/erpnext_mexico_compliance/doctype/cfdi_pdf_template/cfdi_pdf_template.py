b8c6ddcfdfb366be1655cadef2020f7c94cf6885
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2026-06-04 06:52:27 +0000

    style(cfdi_pdf_template): simplify file open mode

--- erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/cfdi_pdf_template/cfdi_pdf_template.py
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/doctype/cfdi_pdf_template/cfdi_pdf_template.py
@@ -36,7 +36,7 @@ def get_sample_file_content(document_type: str, sample_type: t.Literal["xml", "h
 			raise ValueError(f"Unsupported document type: {document_type}")
 
 	path = f"{frappe.get_app_path(app_name)}/examples/cfdi/{file_name}"
-	with open(path, "r") as f:
+	with open(path) as f:
 		content = f.read()
 	return content
 
