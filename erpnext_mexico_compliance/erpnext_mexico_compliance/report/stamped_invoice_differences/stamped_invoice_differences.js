35d8b7bceb3412d1665d17ec1914f5c0cb3a8147
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2026-06-04 06:48:37 +0000

    style(stamped_invoice_differences): standardize code formatting

--- erpnext_mexico_compliance/erpnext_mexico_compliance/report/stamped_invoice_differences/stamped_invoice_differences.js
+++ erpnext_mexico_compliance/erpnext_mexico_compliance/report/stamped_invoice_differences/stamped_invoice_differences.js
@@ -2,13 +2,13 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Stamped Invoice Differences"] = {
-  filters: [
-    {
-      fieldname: "date_range",
-      label: __("Date Range"),
-      fieldtype: "Date Range",
-      reqd: 1,
-      default: [frappe.datetime.month_start(), frappe.datetime.month_end()],
-    },
-  ],
+	filters: [
+		{
+			fieldname: "date_range",
+			label: __("Date Range"),
+			fieldtype: "Date Range",
+			reqd: 1,
+			default: [frappe.datetime.month_start(), frappe.datetime.month_end()],
+		},
+	],
 };
