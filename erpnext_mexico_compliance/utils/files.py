278df9983b589f5b617b4abcda1ccc9f9dc3b8f9
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2026-06-04 06:51:28 +0000

    style(utils): Standardizes indentation to tabs

--- erpnext_mexico_compliance/utils/files.py
+++ erpnext_mexico_compliance/utils/files.py
@@ -7,17 +7,17 @@ import zipfile
 
 
 def compress_files(file_paths: list[str]) -> bytes:
-    """Compresses a list of file paths into a single ZIP file.
+	"""Compresses a list of file paths into a single ZIP file.
 
-    Args:
-        file_paths (list[str]): A list of file paths to be compressed.
+	Args:
+		file_paths (list[str]): A list of file paths to be compressed.
 
-    Returns:
-        bytes: The compressed ZIP file as bytes.
-    """
-    buffer = io.BytesIO()
+	Returns:
+		bytes: The compressed ZIP file as bytes.
+	"""
+	buffer = io.BytesIO()
 
-    with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as z:
-        for file_path in file_paths:
-            z.write(file_path, os.path.basename(file_path))
-    return buffer.getvalue()
+	with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as z:
+		for file_path in file_paths:
+			z.write(file_path, os.path.basename(file_path))
+	return buffer.getvalue()
