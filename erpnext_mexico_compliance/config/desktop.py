5c9c8408d3773872e18efdb4ed44eb6209d5dade
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2026-06-04 06:40:32 +0000

    style(config): Adds blank lines and trailing comma

--- erpnext_mexico_compliance/config/desktop.py
+++ erpnext_mexico_compliance/config/desktop.py
@@ -1,10 +1,11 @@
 from frappe import _
 
+
 def get_data():
 	return [
 		{
 			"module_name": "ERPNext Mexico Compliance",
 			"type": "module",
-			"label": _("ERPNext Mexico Compliance")
+			"label": _("ERPNext Mexico Compliance"),
 		}
 	]
