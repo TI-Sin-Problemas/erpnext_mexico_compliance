03363a84cfe2e03590124396d798f5879969aa46
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2026-06-23 23:28:34 +0000

    feat (migrate): sets missing CFDI status for Sales Invoice and Payment Entry

--- erpnext_mexico_compliance/migrate/__init__.py
+++ erpnext_mexico_compliance/migrate/__init__.py
@@ -6,3 +6,5 @@ def execute_after_migrate_tasks():
 	after_migrate.set_missing_uuids("Payment Entry")
 	after_migrate.enqueue_sat_catalogs_update()
 	after_migrate.set_cfdi_settings()
+	after_migrate.set_missing_cfdi_status("Sales Invoice")
+	after_migrate.set_missing_cfdi_status("Payment Entry")
--- erpnext_mexico_compliance/migrate/after_migrate.py
+++ erpnext_mexico_compliance/migrate/after_migrate.py
@@ -3,9 +3,13 @@ import typing as t
 import click
 import frappe
 from frappe.utils import update_progress_bar
+from rq.timeouts import JobTimeoutException
+from satcfdi.cfdi import CFDI
 
 from erpnext_mexico_compliance import sat
+from erpnext_mexico_compliance.controllers.common import CFDIStatus
 from erpnext_mexico_compliance.utils.cfdi import get_uuid_from_xml
+from erpnext_mexico_compliance.ws_client import get_ws_client, models
 
 
 def _activate_auto_commit(func):
@@ -34,11 +38,10 @@ def set_missing_uuids(doctype: t.Literal["Sales Invoice", "Payment Entry"]):
 
 	query = frappe.qb.get_query(doctype, fields=["name", "mx_stamped_xml"], filters=FILTERS)
 	docs = query.run()
-	total = len(docs)
 	for idx, doc in enumerate(docs):
 		name, xml = doc
 		frappe.db.set_value(doctype, name, "mx_uuid", get_uuid_from_xml(xml))
-		update_progress_bar(f"Setting missing UUIDs for {doctype}", idx, total)
+		update_progress_bar(f"Setting missing UUIDs for {doctype}", idx, len(docs))
 
 
 def enqueue_sat_catalogs_update():
@@ -57,3 +60,90 @@ def set_cfdi_settings():
 	"""Sets the CFDI Stamping Settings to the current site configuration."""
 	settings = frappe.get_single("CFDI Stamping Settings")
 	settings.save(ignore_version=True)
+
+
+CHUNK_SIZE = 500
+
+
+@_activate_auto_commit
+def _set_missing_cfdi_status(doctype: t.Literal["Sales Invoice", "Payment Entry"]):
+	"""Sets the CFDI status of sales invoices and payment entries.
+
+	Args:
+		doctype (t.Literal["Sales Invoice", "Payment Entry"]): The doctype of the documents.
+	"""
+	test_mode = frappe.get_value("CFDI Stamping Settings", None, "test_mode")
+	api = get_ws_client()
+
+	try:
+		while True:
+			docs = frappe.get_all(
+				doctype,
+				fields=["name", "mx_stamped_xml", "cancellation_acknowledgement"],
+				filters={"mx_cfdi_status": ["is", "not set"], "mx_stamped_xml": ["is", "set"]},
+				order_by="name",
+				limit=CHUNK_SIZE,
+			)
+
+			if not docs:
+				break
+
+			for idx, doc in enumerate(docs):
+				ack = doc["cancellation_acknowledgement"]
+				cfdi: CFDI = CFDI.from_string(doc.mx_stamped_xml.encode("utf-8"))  # type: ignore
+
+				if test_mode:
+					status = models.CfdiStatus.DocumentStatus.ACTIVE
+				else:
+					try:
+						status = api.get_status(cfdi).status
+					except Exception:
+						status = models.CfdiStatus.DocumentStatus.ACTIVE
+
+				if status == models.CfdiStatus.DocumentStatus.CANCELLED:
+					value = CFDIStatus.CANCELLED.value
+
+				elif status == models.CfdiStatus.DocumentStatus.ACTIVE and ack:
+					value = CFDIStatus.PENDING_CANCELLATION.value
+
+				elif status == models.CfdiStatus.DocumentStatus.ACTIVE and not ack:
+					value = CFDIStatus.VALID.value
+
+				else:
+					frappe.log_error(f"Failed to set CFDI status for {doctype} {doc.name}")
+					continue
+
+				frappe.db.set_value(doctype, doc.name, "mx_cfdi_status", value)
+
+				update_progress_bar(f"Setting {doctype} missing CFDI Status", idx, len(docs))
+
+			frappe.db.commit()
+	except JobTimeoutException:
+		frappe.enqueue(
+			"erpnext_mexico_compliance.migrate.after_migrate._set_missing_cfdi_status", doctype=doctype
+		)
+
+
+def set_missing_cfdi_status(doctype: t.Literal["Sales Invoice", "Payment Entry"]):
+	"""Sets the CFDI status of sales invoices and payment entries.
+
+	Args:
+		doctype (t.Literal["Sales Invoice", "Payment Entry"]): The doctype of the documents.
+	"""
+	doc_qty = frappe.db.count(
+		doctype, filters={"mx_cfdi_status": ["is", "not set"], "mx_stamped_xml": ["is", "set"]}
+	)
+	if doc_qty > CHUNK_SIZE:
+		q = frappe.enqueue(
+			"erpnext_mexico_compliance.migrate.after_migrate._set_missing_cfdi_status",
+			queue="long",
+			doctype=doctype,
+		)
+		click.echo(
+			f"{doc_qty} {doctype} documents with missing CFDI status have been found, queue {q._id} has been created."
+		)
+
+	elif doc_qty > 0:
+		_set_missing_cfdi_status(doctype)
+	else:
+		return
